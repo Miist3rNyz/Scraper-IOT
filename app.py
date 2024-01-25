f743e75aa9a750b51063180b7d9732a809f4a08a
Miist3rNyz <kevinkesteloot76@gmail.com>
2024-01-25 13:45:04 -0500

    Récupération de la totalité de la collection

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, jsonify
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 import os  # Import the 'os' module
-
-app = Flask(__name__)
-
-# Load environment variables from .env file
 from dotenv import load_dotenv
 
 load_dotenv()
 
+app = Flask(__name__)
+
+# Load environment variables from .env file
+
+
+
 # Access environment variables
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-
-mongo = PyMongo(app)
-
-@app.route('/api/data', methods=['GET'])
+mongodb_uri = os.getenv('MONGODB_URI')
+mongodb_db = os.getenv('MONGODB_DB')
+mongodb_collection = os.getenv('MONGODB_COLLECTION')
+client = MongoClient(mongodb_uri)
+db = client[mongodb_db]
+collection = db[mongodb_collection]
+@app.route('/get_data', methods=['GET'])
 def get_data():
-    collection = mongo.db.votre_collection
-    result = collection.find({}, {"_id": 0})
-    data = [doc for doc in result]
+    # Récupérer les données de la collection
+    data = list(collection.find({}, {'_id': 0}))  # Exclure le champ _id de la réponse JSON si nécessaire
+
+    # Retourner les données en format JSON
     return jsonify(data)
 
 if __name__ == '__main__':

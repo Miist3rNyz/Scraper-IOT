d3d62359feb8290c43ca055a94fafb74d7695637
Ladyflo <95432589+Ladyflo@users.noreply.github.com>
2024-02-11 01:43:42 +0100

    filtre + barre de recherche on point

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const axios = require('axios');
+
 const app = express();
 const port = 31000;
+const url_cve='http://localhost:5000/api/'
+
+var lettre='H'
+
 app.use(express.static(__dirname + '/public'));
 // Configuration du moteur de modèle EJS
 app.set('view engine', 'ejs');
@@ -18,23 +24,71 @@ app.get('/', (req, res) => {
 
 // filtre
 // Route pour le filtrage
-app.post('/filters', (req, res) => {
-
-  const { category, filters } = req.body;
-
-  // Traiter les filtres en fonction de la catégorie (à implémenter )
-  const filteredData = processFilters(category, filters);
-
-  // Renvoyer les filtres traités au format JSON
-  res.json(filteredData);
+app.post('/filters', async (req, res) => {
+  const { category } = req.body;
+  
+  try {
+      const filteredData = await processFilters(category); // Attendre la fin du calcul de processFilters
+      console.log("renvoyé");
+      res.json(filteredData); // Renvoyer les données filtrées au client au format JSON
+  } catch (error) {
+      console.error('Erreur lors du traitement des filtres :', error);
+      res.status(500).send('Erreur lors du traitement des filtres.');
+  }
 });
 
-function processFilters(category, filters) {
-  console.log("liste ok")
-  // Implémentez votre logique de traitement des filtres ici
-  const filteredResults=['filtre 1', 'filtre 2']
-  return filteredResults;
+function processFilters(category) {
+  return new Promise((resolve, reject) => {
+      // Effectuer la requête pour obtenir les données CVE
+      var cpeSet = new Set();
+      console.log(category);
+      axios.get(url_cve + category)
+          .then(response => {
+              const responseData = response.data;
+              var cpeRegex = /cpe:[^"]+/g;
+              responseData.forEach(element => {
+                  str = JSON.stringify(element);
+                  var cpeMatches = str.match(cpeRegex);
+                  if (cpeMatches) {
+                      cpeMatches.forEach(function (cpe) {
+                          var cpeParts = cpe.split(':');
+                          if (cpeParts.length >= 5) {
+                              // Récupérer les parties 3 et 4 (indexés à partir de 0)
+                              var extractedCPE = cpeParts[3] + ":" + cpeParts[4];
+                              // Ajout du CPE à l'ensemble pour éliminer les duplicatas
+                              if (!cpeSet.has(extractedCPE)) {
+                                  cpeSet.add(extractedCPE);
+                              }
+                          }
+                      });
+                  }
+              });
+              console.log("Nombre d'éléments dans cpeSet :", cpeSet.size);
+              resolve(Array.from(cpeSet)); // Résoudre la promesse avec les données filtrées
+          })
+          .catch(error => {
+              console.error('Erreur lors de la requête :', error);
+              reject(error); // Rejeter la promesse en cas d'erreur
+          });
+  });
 }
+
+// Route pour le filtrage
+app.post('/filters', async (req, res) => {
+  const { category } = req.body;
+
+  try {
+      const filteredData = await processFilters(category); // Attendre la fin du calcul de processFilters
+      console.log("renvoyé");
+      res.json(filteredData); // Renvoyer les données filtrées au client au format JSON
+  } catch (error) {
+      console.error('Erreur lors du traitement des filtres :', error);
+      res.status(500).send('Erreur lors du traitement des filtres.');
+  }
+});
+
+ 
+
 
 
 // Démarrage du serveur
--- server/public/script.js
+++ server/public/script.js
@@ -19,37 +19,96 @@ function hidePopup(){
 
 //gestion des filtres
 
-function showFilterPanel(category) {
+function sendCategory(category) {
   console.log("appelé");
-  const filterPanel = document.querySelector('.filter-panel');
-  filterPanel.style.display = 'block';
-
-  // Récupérer les données du formulaire
-  const filterForm = document.getElementById('filter-form');
-  filterForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche le comportement par défaut de soumission du formulaire
   
-    const formData = new FormData(filterForm);
-    const filters = Array.from(formData.getAll('filter'));
-  
-    // Envoyer la catégorie et les valeurs des cases cochées au serveur
-    fetch('/filters', {
+  // Envoyer la catégorie au serveur
+  fetch('/filters', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ category, filters })
-    })
-    .then(response => {
+      body: JSON.stringify({ category })
+  })
+  .then(response => {
       if (response.ok) {
-        console.log('Filters sent successfully.');
-        // Masquer le panneau de filtres après l'envoi
-        filterForm.reset();
-        document.querySelector('.filter-panel').style.display = 'none';
+          console.log('Category sent successfully.');
+          
+          response.json().then(data => {
+            generateFilterElements(data);
+          });
+
       } else {
-        console.error('Failed to send filters.');
+          console.error('Failed to send category.');
       }
-    })
-    .catch(error => console.error('Error:', error));
+  })
+
+  .catch(error => {
+      console.error('Error:', error);
   });
 }
+
+
+function generateFilterElements(filteredData) {
+
+  const filterList = document.getElementById('filter-list');
+  filterList.innerHTML = ''; // Effacer les éléments précédents
+  
+  Object.values(filteredData).forEach(filter => {
+      addFilterElement(filter, filterList); // Passer filterList à la fonction addFilterElement
+  });
+  const panel= document.getElementById('filter-panel')
+  panel.style.display = 'block';
+
+}
+
+// Fonction pour ajouter un élément de filtre
+// Fonction pour ajouter un élément de filtre
+function addFilterElement(filterName, filterList) {
+  const li = document.createElement('li');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+
+  input.type = 'checkbox';
+  input.id = filterName;
+  input.name = 'filter';
+  input.value = filterName;
+
+  label.htmlFor = filterName;
+  label.textContent = filterName;
+
+  li.appendChild(input); // Placer la case à cocher en premier
+  li.appendChild(label); // Placer le texte de l'étiquette après la case à cocher
+
+  filterList.appendChild(li);
+}
+
+
+
+//Recherche
+
+// Sélection de la barre de recherche
+const searchInput = document.getElementById('search-input');
+
+// Sélection de la liste des filtres
+const filterList = document.getElementById('filter-list');
+
+// Écouter les changements dans la barre de recherche
+searchInput.addEventListener('input', function() {
+  const searchText = searchInput.value.toLowerCase(); // Convertir le texte en minuscules
+
+  // Sélectionner tous les éléments de la liste des filtres
+  const filters = filterList.getElementsByTagName('li');
+
+  // Parcourir tous les filtres
+  for (const filter of filters) {
+    const filterName = filter.textContent.toLowerCase(); // Convertir le texte du filtre en minuscules
+    
+    // Vérifier si le filtre correspond au texte de recherche
+    if (filterName.includes(searchText)) {
+      filter.style.display = 'block'; // Afficher le filtre s'il correspond
+    } else {
+      filter.style.display = 'none'; // Masquer le filtre s'il ne correspond pas
+    }
+  }
+});
